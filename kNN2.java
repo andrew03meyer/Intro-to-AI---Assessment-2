43ed2e02f5557fa640df64d01d2ed9da744b7a31
Andrew Meyer <am2660@kent.ac.uk>
2023-12-02 13:25:06 +0000

    cleanup

--- kNN2.java
+++ kNN2.java
@@ -20,11 +20,11 @@ public class kNN2 {
         try{
             File trainingFile = new File("train_data.txt");
             Scanner tfScanner = new Scanner(trainingFile);
-            
+
             int rowIndex = 0;
             int columnIndex;
 
-            while(tfScanner.hasNextLine()){    
+            while(tfScanner.hasNextLine()){
                 columnIndex = 0;
                 //Split line by space and insert into the 2D array
                 String[] temp = tfScanner.nextLine().split(" ");
@@ -48,11 +48,11 @@ public class kNN2 {
         try{
             File testingFile = new File("test_data.txt");
             Scanner tsfScanner = new Scanner(testingFile);
-            
+
             int rowIndex = 0;
             int columnIndex;
 
-            while(tsfScanner.hasNextLine()){    
+            while(tsfScanner.hasNextLine()){
                 columnIndex = 0;
                 //Split line by space and insert into the 2D array
                 String[] temp = tsfScanner.nextLine().split(" ");
@@ -116,6 +116,18 @@ public class kNN2 {
         }
     }
 
+    public static void WriteClassData(String itemClass){
+        try{
+            File classData = new File("output2.txt");
+            FileWriter fwr = new FileWriter(classData);
+            fwr.write(itemClass);
+            fwr.close();
+        }
+        catch(Exception e){
+            System.out.println("File already exists");
+        }
+    }
+
     public static int[] GetOutputLabel(){
         try{
             File outLabelFile = new File("output2.txt");
@@ -138,12 +150,12 @@ public class kNN2 {
             return temp;
         }
     }
-
     /*
      * find the class of item based on kNN (k=5)
      * Parameters - Array of Euclidean distances of each row
      * Return 0/1 based on 5 closest values
      */
+
     public static int getClass(Double[] euclDist){
 
         //Create a HashMap to negate loss of index
@@ -176,21 +188,9 @@ public class kNN2 {
         //Return which class, based on alc & non counts
         if(non > alc){return 0;}else{return 1;}
     }
-
     /*
      * Write the String param to a file (output2.txt)
      */
-    public static void WriteClassData(String itemClass){
-        try{
-            File classData = new File("output2.txt");
-                FileWriter fwr = new FileWriter(classData);
-                fwr.write(itemClass);
-                fwr.close();
-        }
-        catch(Exception e){
-            System.out.println("File already exists");
-        }
-    }
 
     /*
      * Parameters - knn2 output, real classifications
@@ -237,68 +237,7 @@ public class kNN2 {
      * @return - accuracy
      */
     public static void GAColumnComparison(){
-        ArrayList<String> population = new ArrayList<String>();
-        Random rnd = new Random();
-        
-        //Initial population of array
-        for(int x = 0; x < 100; x++){
-            String stringValue = "";
 
-            for(int y=0; y<61; y++){
-                int value = Math.abs(rnd.nextInt() % 2);
-                stringValue += String.valueOf(value) + " ";
-            }
-
-            population.add(stringValue);
-        }
-
-        //Call test function
-        HashMap<Double, String> columnAccuracy = testFunction(population);
-
-        //Call picker
-        ArrayList<String> parents = picker(columnAccuracy);
-
-        //Call evolve
-        evolve(population);
-
-        //20% chance for mutation
-        int rand = rnd.nextInt(100);
-        if(rand <= 100) {
-            parents = mutation(parents);
-        }
-    }
-
-    /*
-    * @param - String ArrayList Population
-    * @calls - EuclideanCompare, CompareLabels, GetTrainingData, GetTestingData, GetTestLabel
-    * Converts population into array
-     */
-    public static HashMap<Double, String> testFunction(ArrayList<String> population){
-        Double[][] trainingData = GetTrainingData();
-        Double[][] testingData = GetTestingData();
-        int[] testLabel = GetTestLabel();
-        HashMap<Double,String> columnAccuracy = new HashMap<Double,String>();
-
-        //Checking each column selection
-        for(String columnSelection : population){
-            //Individual column as string array
-            String[] columnArray = columnSelection.stripTrailing().split(" ");
-            //Parsing string array into int array
-            int[] intColumnArray = new int[columnArray.length];
-            for(int x = 0; x < columnArray.length; x++){
-                intColumnArray[x] = Integer.parseInt(columnArray[x]);
-            }
-
-            //Getting each row classification for that set of columns
-            int[] classification = new int[200];
-            for(int x = 0; x < 200; x++){
-                classification[x] = EuclideanCompare(testingData[x], trainingData, intColumnArray);
-            }
-
-            //putting the column string and the corresponding accuracy in a hashmap
-            columnAccuracy.put(CompareLabels(classification, testLabel), columnSelection);
-        }
-        return columnAccuracy;
     }
 
     /*
@@ -306,34 +245,8 @@ public class kNN2 {
      * @param - HashMap Key: accuracy, Value: column selection
      * @return - String ArrayList containing new parents
      */
-    public static ArrayList<String> picker(HashMap<Double, String> columnAccuracy){
-        ArrayList<Double> values = new ArrayList<Double>();
-        ArrayList<String> parents = new ArrayList<String>();
+    public static void Tournament(){
 
-        //find the sum
-        Double sum = Double.parseDouble("0");
-        for(Double temp : columnAccuracy.keySet()){
-            sum += temp;
-            values.add(temp);
-        }
-
-        //pick 100 values (based on chance)
-        for(int x = 0; x < 100; x++){
-            int index = 0;
-            Random rnd1 = new Random();
-            Double randNum = rnd1.nextDouble() * sum;
-            Double partSum = Double.parseDouble("0");
-
-            //repeat until found value
-            while(partSum < randNum){
-                partSum += values.get(index);
-                index++;
-            }
-
-            ArrayList<String> temp = new ArrayList<String>();
-            parents.add(columnAccuracy.get(values.get(index-1)));         //add the key
-        }
-        return parents;
     }
 
     /*
@@ -341,7 +254,7 @@ public class kNN2 {
      * @param - 2D array of parents
      * @return - 2D array of new parents
      */
-    public static ArrayList<String> evolve(ArrayList<String> population){
+    public static ArrayList<String> Evolve(ArrayList<String> population){
         ArrayList<String> parents = new ArrayList<String>();
 
         //Split population into two
@@ -371,14 +284,14 @@ public class kNN2 {
     }
 
     /*
-     * Mutate 20 rows
+     * Mutate 50 rows
      * @param - 2D ArrayList of columns
      * @return - 2D ArrayList of mutated columns
      */
-    public static ArrayList<String> mutation(ArrayList<String> population){
+    public static ArrayList<String> Mutate(ArrayList<String> population){
         System.out.println("In mutation method");
-        for(int x = 0; x < 20; x++){
-            System.out.println(population.get(x));
+        for(int x = 0; x < 5; x++){
+            //System.out.println(population.get(x));
             String selection = population.get(x).replaceAll("\\s+", "");
             String newSelection = "";
 
